bbf3dedaea95df44e70b9021d30c2e8c11eb7e42
devAbbasAlasgarov <devAbbasAlasgarov@gmail.com>
2022-09-11 15:18:13 +0400

    Update Program.cs
    Test 1

--- Chapter6Exercise/Program.cs
+++ Chapter6Exercise/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using System.Globalization;
+// using Nakov.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Chapter6ExercisesSolutions
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Chapter 6 Exercise
+
+            // Factorial Finder Program with do-while loop
+            Console.Write("n = ");
+            int number = int.Parse(Console.ReadLine());
+
+            decimal factorial = 1;
+
+            do
+            {
+                if (number < 1)
+                {
+                    break;
+                }
+                factorial *= number;
+                number--;
+            } while (number > 0);
+            Console.WriteLine("number! = " + factorial);
+
+            // Factorial Finder Program with while loop
+            /*
+
+                int number = int.Parse(Console.ReadLine());
+
+                int factorial = 1;
+
+                while (true)
+                {
+                    if (number <= 1)
+                    {
+                        break;
+                    }
+
+                    factorial *= number;
+                    number--;
+                }
+                Console.WriteLine("number! = " + factorial);
+            */
+        }
+    }
+}
 
