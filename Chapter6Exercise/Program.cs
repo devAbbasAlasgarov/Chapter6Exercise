1c913242762f78f81800bd80debe5c8c82ea73dd
devAbbasAlasgarov <devAbbasAlasgarov@gmail.com>
2022-09-14 16:59:35 +0400

    Update Program.cs

--- Chapter6Exercise/Program.cs
+++ Chapter6Exercise/Program.cs
@@ -18,8 +18,32 @@ namespace Chapter6Exercises
 
             // Chapter 6 Exercise
 
-            // Luck numbers ABCD A+B = C+D
+            // Lottery 6/49
 
+            for(int i1 = 1; i1 <= 44; i1++)
+            {
+                for(int i2 = i1 + 1; i2 <= 45; i2++)
+                {
+                    for(int i3 = i2 + 1; i3 <= 46; i3++)
+                    {
+                        for (int i4 = i3 + 1; i4 <= 47; i4++)
+                        {
+                            for (int i5 = i4 + 1; i5 <= 48; i5++)
+                            {
+                                for (int i6 = i5 + 1; i6 <= 49; i6++)
+                                {
+                                    Console.WriteLine(i1 + " " + i2 + " " +
+                                        i3 + " " + i4 + " " + i5 + " " + i6);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.Read();
+
+            // Luck numbers ABCD A+B = C+D
+            /*
             for(int a = 0; a <= 9; a++)
             {
                 for(int b = 1; b <= 9; b++)
@@ -37,6 +61,7 @@ namespace Chapter6Exercises
                 }
             }
             Console.Read();
+            */
 
             // Prime Numbers in an Interval
             /*
